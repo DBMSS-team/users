7fecbf5532ab4ed95b8f26bce0383e7e5acddec1
I355102 <sahil.kumar@sap.com>
2020-08-19 00:30:41 +0530

    files updated

--- db/models/user.model.js
+++ db/models/user.model.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-var uuid = require('node-uuid');
+const uuid = require('node-uuid');
 
 const Schema = mongoose.Schema;
 
@@ -18,7 +18,7 @@ let addressSchema = new Schema({
 	city: { type: String, required: [true, 'Insert city'] },
 	state: { type: String, required: [true, 'Insert state'] },
 	country: { type: String, required: [true, 'Insert country'] },
-	pin_code: { Number, required: [true, 'Insert pin code'] },
+	pin_code: { type: Number, required: [true, 'Insert pin code'] },
 });
 
 let cardSchema = new Schema({
@@ -41,29 +41,30 @@ let cardSchema = new Schema({
 	},
 });
 
-let userSchema = new Schema({
-	_id: {
-		type: String,
-		default: function genUUID() {
-			uuid.v1();
+let userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			unique: true,
+			required: true,
+			index: { unique: true },
 		},
+		phone_number: { type: Number, required: true },
+		email: { type: String, required: true },
+		user_category: {
+			type: String,
+			default: 'Normal',
+			text: 'Normal and Premium',
+		}, // 2 types: Normal and Premium
+		wallet_id: { type: String },
+		user_addresses: [addressSchema],
+		cards: [cardSchema],
 	},
-	username: {
-		type: String,
-		unique: true,
-		required: true,
-		index: { unique: true },
-	},
-	phone_number: { type: Number, required: true },
-	email: { type: String },
-	user_category: { type: String, default: 'Normal' },
-	wallet_id: { type: String },
-	user_addresses: [addressSchema],
-	cards: [cardSchema],
-	created_on: { type: Date, default: Date.now },
-	updated_on: { type: Date, default: Date.now },
-});
+	{
+		timestamps: true,
+	}
+);
 
-const Users = mongoose.model('Users', userSchema);
+const User = mongoose.model('User', userSchema);
 
-module.exports = Users;
+module.exports = { User, addressSchema };
