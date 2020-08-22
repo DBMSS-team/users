6d7f70a37a04e9ad429f2c2b79cce7a814d8b0c0
I355102 <sahil.kumar@sap.com>
2020-08-22 21:09:43 +0530

    define unit test cases

---
+++ tests/user.test.js
@@ -0,0 +1,56 @@
+const mongoose = require('mmongoose');
+const UserModel = require('../db/models/user.model');
+const { describe } = require('yargs');
+const userData = {
+	username: 'Sahil',
+	password: 'Sahil',
+	email: 'kumarsahil129@gmail.com',
+	user_category: 'Premium',
+};
+
+describe('User model test', () => {
+	// Connect to mongoDB
+	beforeAll(async () => {
+		await mongoose.connect(
+			global.__MONGO_URI__,
+			{ useNewUrlParser: true, useCreateIndex: true },
+			(err) => {
+				if (err) {
+					console.error(err);
+					process.exit(1);
+				}
+			}
+		);
+	});
+
+	it('create and save user', async () => {
+		const validUser = new UserModel(userData);
+		const savedUser = await validUser.save();
+		// Object Id should be defined when successfully saved to MongoDB.
+		expect(savedUser._id).toBeDefined();
+		expect(savedUser.username).toBe(userData.username);
+		expect(savedUser.email).toBe(userData.email);
+		expect(savedUser.user_category).toBe(userData.user_category);
+	});
+
+	it('check if the existing user is authenticated', async () => {
+		const validUser = new UserModel({
+			username: userData.username,
+			password: userData.password,
+		});
+		validUser.getAuthenticated((err, user, reason) => {
+			expect(err).toBeUndefined();
+			expect(validUser.username).toBe(user.username);
+		});
+	});
+
+	it('check if the incorrect user is not authenticated', async () => {
+		const validUser = new UserModel({
+			username: 'WrongUserAbcd',
+			password: 'abcdefgh',
+		});
+		validUser.getAuthenticated((err, user, reason) => {
+			expect(err).toBeDefined();
+		});
+	});
+});
