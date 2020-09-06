f556b4ff234606d882e73da4548bbb2125f19c31
I355102 <sahil.kumar@sap.com>
2020-09-06 14:13:21 +0530

    eslint errors fixed

--- srv/routes/user.js
+++ srv/routes/user.js
@@ -1,59 +1,61 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const authorization = require(__commons).authorization.authorizationMiddleware;
-let User = require('../../db/models/user.model').User;
+const User = require("../../db/models/user.model").User;
 
 router.use(authorization);
 
 // Get all users
-router.route('/').get((req, res) => {
+router.route("/").get((req, res) => {
 	User.find()
 		.then((user) => res.json(user))
-		.catch((err) => res.status(400).json('Error: ' + err));
+		.catch((err) => res.status(400).json("Error: " + err));
 });
 
 // Get specific user
-router.get('/:id', (req, res) => {
+router.get("/:id", (req, res) => {
 	const id = req.params.id;
 	User.findById(id, (err, user) => {
-		if (err) res.status(400).json('Error: ' + err);
+		if (err) { res.status(400).json("Error: " + err); }
 		res.json(user);
 	});
 });
 
 // Create new user
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
 	try {
-		const userExists = await User.find({ username: req.body.username });
+		const userExists = await User.find({
+			username: req.body.username
+		});
 		if (userExists.length) {
-			res.status(400).json('User already exists with this username');
+			res.status(400).json("User already exists with this username");
 			return;
 		}
 		const newUser = new User(req.body);
 		newUser
 			.save()
-			.then(() => res.status(200).json('User added.'))
-			.catch((err) => res.status(400).json('Error: ' + err));
+			.then(() => res.status(200).json("User added."))
+			.catch((err) => res.status(400).json("Error: " + err));
 	} catch (err) {
-		res.status(400).json('Error: ' + err);
+		res.status(400).json("Error: " + err);
 	}
 });
 
 // Update a specific user
-router.put('/:id', async (req, res) => {
+router.put("/:id", async (req, res) => {
 	const id = req.params.id;
 	try {
-		let updatedUser = await User.findByIdAndUpdate(id, req.body, {
-			new: true,
-			useFindAndModify: false,
+		const updatedUser = await User.findByIdAndUpdate(id, req.body, {
+			"new": true,
+			useFindAndModify: false
 		});
 		res.json(updatedUser);
 	} catch (err) {
-		res.status(400).json('Error: ' + err);
+		res.status(400).json("Error: " + err);
 	}
 });
 
 // Delete a user
-router.delete('/:id', async (req, res, next) => {
+router.delete("/:id", async (req, res, next) => {
 	const id = req.params.id;
 	try {
 		const deletedUser = await User.findByIdAndDelete(id);
@@ -62,7 +64,7 @@ router.delete('/:id', async (req, res, next) => {
 		if (err.status) {
 			next(err);
 		}
-		res.status(400).json('Error: ' + err);
+		res.status(400).json("Error: " + err);
 	}
 });
 
--- tests/user.test.js
+++ tests/user.test.js
@@ -2,57 +2,57 @@ global.__commons = __dirname + '/../srv/commons';
 const mongoose = require('mongoose');
 const UserModel = require('../db/models/user.model').User;
 const userData = {
-	username: 'Sahil',
-	password: 'Sahil',
-	email: 'kumarsahil129@gmail.com',
-	user_category: 'Premium',
+  username: 'Sahil',
+  password: 'Sahil',
+  email: 'kumarsahil129@gmail.com',
+  user_category: 'Premium',
 };
 
 describe('User model test', () => {
-	// Connect to mongoDB
-	beforeAll(async () => {
-		await mongoose.connect(
-			global.__MONGO_URI__,
-			{ useNewUrlParser: true, useCreateIndex: true },
-			(err) => {
-				if (err) {
-					console.error(err);
-					process.exit(1);
-				}
-			}
-		);
-	});
+  // Connect to mongoDB
+  beforeAll(async () => {
+    await mongoose.connect(
+      global.__MONGO_URI__,
+      {useNewUrlParser: true, useCreateIndex: true},
+      (err) => {
+        if (err) {
+          console.error(err);
+          process.exit(1);
+        }
+      }
+    );
+  });
 
-	test('create and save user', async () => {
-		const validUser = new UserModel(userData);
-		const savedUser = await validUser.save();
-		// Object Id should be defined when successfully saved to MongoDB.
-		expect(savedUser._id).toBeDefined();
-		expect(savedUser.username).toBe(userData.username);
-		expect(savedUser.email).toBe(userData.email);
-		expect(savedUser.user_category).toBe(userData.user_category);
-	});
+  test('create and save user', async () => {
+    const validUser = new UserModel(userData);
+    const savedUser = await validUser.save();
+    // Object Id should be defined when successfully saved to MongoDB.
+    expect(savedUser._id).toBeDefined();
+    expect(savedUser.username).toBe(userData.username);
+    expect(savedUser.email).toBe(userData.email);
+    expect(savedUser.user_category).toBe(userData.user_category);
+  });
 
-	test('check if the existing user is authenticated', async () => {
-		const validUser = new UserModel({
-			username: userData.username,
-			password: userData.password,
-		});
-		validUser.getAuthenticated((err, user, reason) => {
-			expect(err).toBeUndefined();
-			expect(validUser.username).toBe(user.username);
-		});
-	});
+  test('check if the existing user is authenticated', async () => {
+    const validUser = new UserModel({
+      username: userData.username,
+      password: userData.password,
+    });
+    validUser.getAuthenticated((err, user, reason) => {
+      expect(err).toBeUndefined();
+      expect(validUser.username).toBe(user.username);
+    });
+  });
 
-	test('check if the incorrect user is not authenticated', async () => {
-		const validUser = new UserModel({
-			username: 'WrongUserAbcd',
-			password: 'abcdefgh',
-		});
-		validUser.getAuthenticated((err, user, reason) => {
-			expect(err).toBeDefined();
-		});
-	});
+  test('check if the incorrect user is not authenticated', async () => {
+    const validUser = new UserModel({
+      username: 'WrongUserAbcd',
+      password: 'abcdefgh',
+    });
+    validUser.getAuthenticated((err, user, reason) => {
+      expect(err).toBeDefined();
+    });
+  });
 
-	afterAll();
+  afterAll();
 });
